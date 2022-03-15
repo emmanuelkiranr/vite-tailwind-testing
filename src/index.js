3c9c65e35a150412b8e58a8ff5c21c623982318b
emmanuelkiranr <emmanuelkiranr@gmail.com>
2022-03-15 22:13:26 +0530

    changed script path in index.hmtl

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-// var loader = setInterval(function () {
-//   if (document.readyState !== "complete") return;
-//   clearInterval(loader);
-//   const menu = document.querySelector("#menu");
-//   const burger = document.querySelector("#burger");
-//   burger.addEventListener("click", () => {
-//     if (menu.classList.contains("hidden")) {
-//       menu.classList.remove("hidden");
-//     } else {
-//       menu.classList.add("hidden");
-//     }
-//   });
-// }, 300);
+var loader = setInterval(function () {
+  if (document.readyState !== "complete") return;
+  clearInterval(loader);
+  const menu = document.querySelector("#menu");
+  const burger = document.querySelector("#burger");
+  burger.addEventListener("click", () => {
+    if (menu.classList.contains("hidden")) {
+      menu.classList.remove("hidden");
+    } else {
+      menu.classList.add("hidden");
+    }
+  });
+}, 300);
